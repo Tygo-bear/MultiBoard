2952e0cea7fc3c50df78943c6e8a870506879d55
Tygo <26803517+Casperke@users.noreply.github.com>
2019-02-08 09:47:25 +0100

    fix #14
    the running app wil popup when and second instance is started

--- MultiBoard/Form1.cs
+++ MultiBoard/Form1.cs
@@ -463,5 +463,29 @@ namespace MultiBoard
             backgroundWorker2.RunWorkerAsync();
 
         }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == NativeMethods.WM_SHOWME)
+            {
+                ShowMe();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void ShowMe()
+        {
+            this.Show();
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+            // get our current "TopMost" value (ours will always be false though)
+            bool top = TopMost;
+            // make our form jump to the top of everything
+            TopMost = true;
+            // set it back to whatever it was
+            TopMost = top;
+        }
     }
 }
--- MultiBoard/Program.cs
+++ MultiBoard/Program.cs
@@ -10,38 +10,26 @@ namespace MultiBoard
 {
     static class Program
     {
-        private static string appGuid = "8c14eced-65ff-49d3-9077-a8c95aa2a054";
-
+        static Mutex mutex = new Mutex(true, "{8c14eced-65ff-49d3-9077-a8c95aa2a054}");
         [STAThread]
         static void Main()
         {
-            using (Mutex mutex = new Mutex(false, @"Global\" + appGuid))
+            if (mutex.WaitOne(TimeSpan.Zero, true))
             {
-                if (!mutex.WaitOne(0, false))
-                {
-                    MessageBox.Show("Instance already running");
-                    return;
-                }
-
-                GC.Collect();
-
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new MultiBoard());
+                mutex.ReleaseMutex();
             }
-        }
-
-        [DllImport("USER32.DLL")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-        static private void ThreadFormVisable(Form frm)
-        {
-            if (frm != null)
+            else
             {
-                // display the form and bring to foreground.
-                frm.Visible = true;
-                frm.WindowState = FormWindowState.Normal;
-                frm.Show();
-                SetForegroundWindow(frm.Handle);
+                // send our Win32 message to make the currently running instance
+                // jump on top of all the other windows
+                NativeMethods.PostMessage(
+                    (IntPtr)NativeMethods.HWND_BROADCAST,
+                    NativeMethods.WM_SHOWME,
+                    IntPtr.Zero,
+                    IntPtr.Zero);
             }
         }
     }
