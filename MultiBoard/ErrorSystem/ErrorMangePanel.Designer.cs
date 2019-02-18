d688072a47b550473372f6eb30c6673b19b200af
Tygo <26803517+Casperke@users.noreply.github.com>
2019-02-18 11:51:04 +0100

    error system

---
+++ MultiBoard/ErrorSystem/ErrorMangePanel.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MultiBoard.ErrorSystem
+{
+    partial class ErrorMangePanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ERROR_LIST_LISTBOX = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(854, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Errors";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 475);
+            this.panel2.TabIndex = 1;
+            // 
+            // ERROR_LIST_LISTBOX
+            // 
+            this.ERROR_LIST_LISTBOX.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ERROR_LIST_LISTBOX.FormattingEnabled = true;
+            this.ERROR_LIST_LISTBOX.Location = new System.Drawing.Point(200, 31);
+            this.ERROR_LIST_LISTBOX.Name = "ERROR_LIST_LISTBOX";
+            this.ERROR_LIST_LISTBOX.Size = new System.Drawing.Size(654, 475);
+            this.ERROR_LIST_LISTBOX.TabIndex = 2;
+            // 
+            // ErrorMangePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ERROR_LIST_LISTBOX);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ErrorMangePanel";
+            this.Size = new System.Drawing.Size(854, 506);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox ERROR_LIST_LISTBOX;
+    }
+}
---
+++ MultiBoard/ErrorSystem/Errors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace MultiBoard.ErrorSystem
+{
+    class Errors
+    {
+        private List<string> _errorMessage = new List<string>();
+        private List<string> _errorCode = new List<string>();
+
+        public void clearErrors()
+        {
+            _errorMessage.Clear();
+            _errorCode.Clear();
+        }
+
+        public void addError(string errorCode, string errorMessage)
+        {
+            _errorCode.Add(errorCode);
+            _errorMessage.Add(errorMessage);
+        }
+
+        public List<string> getErrorMessages()
+        {
+            return _errorMessage;
+        }
+
+        public List<string> getErrorCodes()
+        {
+            return _errorCode;
+        }
+
+        public void deleteByIndex(int index)
+        {
+            _errorCode.RemoveAt(index);
+            _errorMessage.RemoveAt(index);
+        }
+
+    }
+}
