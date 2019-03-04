d52f169273a8ee3e7fd9e783cb95a23d992ae80c
Tygo <26803517+Casperke@users.noreply.github.com>
2019-03-04 14:46:10 +0100

    removed start message

--- USBHIDBootKbd/USBHIDBootKbd.ino
+++ USBHIDBootKbd/USBHIDBootKbd.ino
@@ -116,9 +116,7 @@ void setup()
 {
   Serial.begin( 115200 );
 #if !defined(__MIPSEL__)
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
-  Serial.println("Start");
 
   if (Usb.Init() == -1)
     Serial.println("ERROR:0001"); //OSC did not start.
