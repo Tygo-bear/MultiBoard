13e39578f11a0af277a1a76eeb7c87473ff2dbc1
Tygo <26803517+Casperke@users.noreply.github.com>
2019-06-07 15:01:58 +0200

    V1.0.1.3 update

--- MultiBoard/MultiBoard/MainForm.Designer.cs
+++ MultiBoard/MultiBoard/MainForm.Designer.cs
@@ -83,7 +83,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Beta V1.0.1.2";
+            this.label2.Text = "Beta V1.0.1.3";
             this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDown);
             // 
             // MINIMIZE_B
